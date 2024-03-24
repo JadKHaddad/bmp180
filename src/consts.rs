f1ca87b4d1dcfb3188b586640d65c9cf74f4a980
JadKHaddad <jadkhaddad@gmail.com>
2024-03-24 20:30:40 +0100

    added feature: log
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/consts.rs
+++ src/consts.rs
@@ -5,16 +5,6 @@ pub const BMP180_ID: u8 = 0x55;
 pub const BMP180_REGISTER_CHIPID: u8 = 0xD0;
 
 pub const BMP180_CAL_AC1: u8 = 0xAA;
-pub const BMP180_CAL_AC2: u8 = 0xAC;
-pub const BMP180_CAL_AC3: u8 = 0xAE;
-pub const BMP180_CAL_AC4: u8 = 0xB0;
-pub const BMP180_CAL_AC5: u8 = 0xB2;
-pub const BMP180_CAL_AC6: u8 = 0xB4;
-pub const BMP180_CAL_B1: u8 = 0xB6;
-pub const BMP180_CAL_B2: u8 = 0xB8;
-pub const BMP180_CAL_MB: u8 = 0xBA;
-pub const BMP180_CAL_MC: u8 = 0xBC;
-pub const BMP180_CAL_MD: u8 = 0xBE;
 
 pub const BMP180_CONTROL: u8 = 0xF4;
 pub const BMP180_TEMPDATA: u8 = 0xF6;
